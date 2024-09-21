136baefc1529451a2b5091d8ccebe4ce4e5b3214
Cem Urgen <cem35520@gmail.com>
2024-09-21 09:04:21 -0400

    Complete exercises 05-07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (numOne < 0 || !Number.isInteger(numOne) ||
+        numTwo < 0 || !Number.isInteger(numTwo)) {
+        return 'ERROR';
+    }
 
+    let start = numOne;
+    let end = numTwo;
+    if (numOne > numTwo) {
+        start = numTwo;
+        end = numOne;
+    }
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
