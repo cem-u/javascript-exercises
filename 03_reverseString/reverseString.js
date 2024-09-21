c105f741973a86ba9c280c0b664e4baacca19b0d
Cem Urgen <cem35520@gmail.com>
2024-09-20 22:49:02 -0400

    Complete exercises 01-04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let n = string.length;
 
+    stringArr = string.split("");
+    reverseStringArr = [];
+
+    for (let i = 0; i < n; i++) {
+        reverseStringArr[i] = stringArr[(n - 1) - i];
+    }
+
+    return reverseStringArr.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = arr.length - 1; j >= 0; j--) {
+            if (arr[j] === arguments[i]) {
+                arr.splice(j, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
